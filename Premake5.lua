8831080ebaf5ca98ce4383247d2e69d31e32bee0
hwx0000 <wutong@papegames.net>
2023-03-23 13:42:29 +0800

    Add Premake5.lua file

---
+++ Premake5.lua
@@ -0,0 +1,37 @@
+project "2DPhysicsEngine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines
+	{
+	}
+
+	includedirs
+	{
+		"include"
+	}
+	
+
+	files
+	{
+		"include/**.h",
+		"src/**.cpp"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off" 
